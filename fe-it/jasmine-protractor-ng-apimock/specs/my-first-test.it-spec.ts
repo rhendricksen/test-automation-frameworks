d761d974de98c0285745b54ad68c92877a2bfe31
richardhendricksen <richard.hendricksen@gmail.com>
2019-01-30 15:42:41 +0100

    added typescript linting

---
+++ fe-it/jasmine-protractor-ng-apimock/specs/my-first-test.it-spec.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+
+describe('My first test', () => {
+
+  describe('Open x', () => {
+
+    describe('When something something', () => {
+      beforeAll(async () => {
+      });
+
+      it('Then it should show the digiboard tool button', async () => {
+          console.log('hello world');
+
+      });
+    });
+
+    describe('When the digiboard-tool is opened', () => {
+      beforeAll(async () => {
+
+      });
+
+      it('Then it should show the tool', async () => {
+
+      });
+
+    });
+  });
+
+});
